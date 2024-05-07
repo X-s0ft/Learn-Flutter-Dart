ea5537432abc445fd38c060925dcbc94b7d82cc8
Данила <d4vanchik@yandex.ru>
2024-05-07 21:07:40 +0300

    Start_2

--- Base_Dart/LetCodeDart.dart
+++ Base_Dart/LetCodeDart.dart
@@ -1,20 +1,28 @@
-/*Number 1 'Two Sum'*/
 void main(){
-  Soulution first = Soulution();
-  print(first.twoSum([2,7, 11, 15], 9));
-  print(first.twoSum([3,2, 4], 6));
-  print(first.twoSum([3, 3], 6));
+  /*Number 1 'Two Sum'*/
+  // Soulution first = Soulution();
+  // print(first.twoSum([2,7, 11, 15], 9));
+  // print(first.twoSum([3,2, 4], 6));
+  // print(first.twoSum([3, 3], 6));
+  /* Number 2 'Palindrome Numbers'*/
+  print(isPalindrome(121));
+  print(isPalindrome(-121));
+  print(isPalindrome(10));
 }
 
-class Soulution{
-  List<int>? twoSum(List<int> nums, int target) {
-    for (int i = 0; i < nums.length; i++){
-      int currentElement = nums[i];
-      final lio = nums.lastIndexOf(target - currentElement);
-      if (lio != i && lio != -1){
-        return [i, lio];
-      }
-    }
-    return null;
-  }
+// class Soulution{
+//   List<int>? twoSum(List<int> nums, int target) {
+//     for (int i = 0; i < nums.length; i++){
+//       int currentElement = nums[i];
+//       final lio = nums.lastIndexOf(target - currentElement);
+//       if (lio != i && lio != -1){
+//         return [i, lio];
+//       }
+//     }
+//     return null;
+//   }
+// }
+bool isPalindrome(int x){
+  return x > 0 ? true : false;
 }
+
